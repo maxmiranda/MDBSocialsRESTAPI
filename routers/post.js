e1fd58d6e85a4e7da49e456cb4a8aea38b5525d8
Max Miranda <maxmiranda@berkeley.edu>
2018-03-15 00:12:15 -0700

    changed one line

--- routers/post.js
+++ routers/post.js
@@ -45,7 +45,7 @@ router.post("/posts", function(req, res) {
   req.checkBody("posterId", "no postId passed").notEmpty();
   req.checkBody("posterId", "posterId does not exist").isValidId(userRef);
   console.log("about to createByAutoId");
-  console.log(req);
+  console.log("printing req");
   completeRequest(req, res, postLogic.createByAutoId);
 });
 
